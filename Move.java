b23609ac3aef6cbbf8c1e90536f135265ce0101c
Shanjian2311 <shanjianz@nycstudents.net>
2025-06-04 11:56:26 -0400

    Update Move.java

--- Move.java
+++ Move.java
@@ -1,7 +1,25 @@
+package FinchPackage;
 public class Move{
 	public static void main(String[] args) {
 		Finch f1 = new Finch();
+		
 		f1.setMotors(10, 10);
-		f1.setMove("F", 10, 0);
+		
+		while(f1.getDistance() < 5) {
+			f1.setMove("R", 5, 5);
 		}
-}
+		
+		
+		f1.setMove("L", 10, 10);
+		f1.pause(3);
+		f1.setMove("R", 10, 10);
+		
+		
+		if(f1.getDistance() < 3) {
+			f1.stop();
+		}
+		
+		f1.stop();
+		
+		}
+	}
