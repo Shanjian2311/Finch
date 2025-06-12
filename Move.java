e9d83ac120c6f7e21c7c6e765707ff0fc8970015
Shanjian2311 <shanjianz@nycstudents.net>
2025-06-12 11:17:52 -0400

    Update Move.java

--- Move.java
+++ Move.java
@@ -5,15 +5,15 @@ public class Move{
 		f1.setMotors(10, 10);
 		
 		if(f1.getDistance() < 5) {
-			f1.setMove("B",10, 10);
-			f1.setBeak(100,0,0);
-			f1.setTurn("R", 90, 5);
+			f1.setMove("B",10, 10); //Move backward increase the distance//
+			f1.setBeak(100,0,0); //shows red led lights as alret sign//
+			f1.setTurn("R", 90, 5); //turn right 90 degree//
 		}
 
 		
 		
 		if(f1.getDistance() <= 3) {
-			f1.stop();
+			f1.stop(); 
 		}
 		
 		f1.stop();
